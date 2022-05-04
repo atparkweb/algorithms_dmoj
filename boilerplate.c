a0dd28ce3fa45876a4cfce97a2dbdac52079e6e7
Andrew Park <atpark333@gmail.com>
2022-05-05 00:25:25 +0900

    Add boilerplate file

---
+++ boilerplate.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+  printf("Hello, world.");
+  return 0;
+}
+
+// Calls to `free` are omitted in examples. Feel free to use it ;) 
