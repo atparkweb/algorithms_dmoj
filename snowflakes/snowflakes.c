a84b6dd33f775cbd34545b5a2d737df04ed21755
Andrew Park <atpark333@gmail.com>
2022-05-08 23:32:26 +0900

    refactor: Move implementations of snowflakes from header to main file

--- snowflakes/snowflakes.c
+++ snowflakes/snowflakes.c
@@ -3,6 +3,66 @@
 
 #define SIZE 100000
 
+// Node struct for Linked List
+typedef struct snowflake_node {
+  int snowflake[6];
+  struct snowflake_node *next;
+} snowflake_node;
+
+int identical_right(int snow1[], int snow2[], int start) {
+  int offset;
+  for (offset = 0; offset < 6; offset++) {
+    if (snow1[offset] != snow2[(start + offset) % 6]) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int identical_left(int snow1[], int snow2[], int start) {
+  int offset, snow2_index;
+  for (offset = 0; offset < 6; offset++) {
+    snow2_index = start - offset;
+    if (snow2_index < 0) {
+      snow2_index = snow2_index + 6;
+    }
+    if (snow1[offset] != snow2[snow2_index]) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int are_identical(int snow1[], int snow2[]) {
+  int start;
+  for (start = 0; start < 6; start++) {
+    if (identical_right(snow1, snow2, start)) {
+      return 1;
+    }
+    if (identical_left(snow1, snow2, start)) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void identify_identical(int snowflakes[][6], int n) {
+  int i, j;
+  for (i = 0; i < n; i++) {
+    for (j = i+1; j < n; j++) {
+      if (are_identical(snowflakes[i], snowflakes[j])) {
+        printf("Twin snowflakes have been found.\n");
+        return;
+      }
+    }
+  }
+  printf("No two snowflakes are alike.\n");
+}
+
+int code(int snowflake[]) {
+  return (snowflake[0] + snowflake[1] + snowflake[2] + snowflake[3] + snowflake[4] + snowflake[5]) % SIZE;
+}
+
 int main(void) {
   int snowflakes[SIZE][6];
   int n, i, j;
